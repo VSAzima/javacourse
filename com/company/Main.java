e15efe9a34b51b997dbc831fb933a7f9ea47a081
VSAzima <amandahmond@gmail.com>
2020-06-08 19:39:53 +0400

    module 3 hello world

---
+++ com/company/Main.java
@@ -0,0 +1,79 @@
+package com.company;
+
+/*public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+	System.out.println("Hello World!");
+    }
+}
+*/
+/*public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i= i + 1;
+        System.out.println(i);
+        System.out.println("Hello World");
+    }
+}
+ */
+/*public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i= i + 1;
+        System.out.println(i);
+        System.out.println("Hello World");
+    }
+}
+ */
+/*public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("sum = " + c);
+    }
+}
+*/
+/*public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+    }
+}
+*/
+/*public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
+*/
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
+
+
+
